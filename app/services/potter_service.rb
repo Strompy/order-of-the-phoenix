a0ec3c2f2dac1a47b4a1f0c66e001cbe4a4aa060
Phillip Strom <phillip.strom@gmail.com>
2020-09-01 10:03:06 -0600

    Create PotterService

---
+++ app/services/potter_service.rb
@@ -0,0 +1,22 @@
+class PotterService
+  def self.search(house)
+    members = get_json("characters?orderOfThePhoenix=true&house=#{house}")
+    members.map do |member|
+      Character.create(member)
+    end
+  end
+
+  private
+
+  def self.get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new('https://www.potterapi.com/v1') do |faraday|
+      faraday.params[:key] = ENV['POTTER_API_KEY']
+    end
+  end
+
+end
